2b731e62e50a6f7bc9659eb46f5008f0214affaa
Larry Zhou <larry@Larrys-MacBook-Pro.local>
2018-03-28 20:26:02 -0400

    added database connection

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fisher.Bookstore.Api.Models{
 
     public class Author{
 
         public int Id { get; set;}
-        public string Fname { get; set; }
-        public string Lname { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public List<Book> Titles { get; set;}
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -6,7 +6,7 @@ namespace Fisher.Bookstore.Api.Models{
 
         public int Id { get; set;}
         public string Title { get; set; }
-        public string Author { get; set; }
+        public Author Author { get; set; }
         public string ISBN { get; set; }
         public DateTime PublishDate { get; set; }
         public string Publisher { get; set; }
